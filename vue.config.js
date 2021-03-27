62d163a1b0f198b9d3bbe5fddcc6cc620a60aaa4
xuanqb <1128767745>
2021-03-27 23:44:43 +0800

    修改代码

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    devServer: {
-        'open': true
-    }
-
+  devServer: {
+    open: true
+  },
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue_shop/' : '/'
 }
